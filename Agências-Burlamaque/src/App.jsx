22ca013ecadb7c05d73a2b884f6f4cc4a6190bb0
Tiago265 <futlastf998@gmail.com>
2025-05-24 10:10:04 -0300

    'Navbar'

---
+++ Agências-Burlamaque/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import Navbar from './components/Navbar'
+
+function App() {
+
+  return (
+    <div className='container-app'>
+      <Navbar />
+    </div>
+  )
+}
+
+export default App
